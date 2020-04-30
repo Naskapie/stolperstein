3c862f87937cbf9a16644513c6fb763640d422aa
Hendrik Dalagan <naskapie@gmx.net>
2020-04-30 16:18:17 +0200

    Delete comments

--- lib/theme/style.dart
+++ lib/theme/style.dart
@@ -3,11 +3,6 @@ import 'package:flutter/material.dart';
 ThemeData appTheme() {
   return ThemeData(
     primarySwatch: Colors.grey,
-    // // buttonColor: Colors.white,
-    // buttonTheme: const ButtonThemeData(
-    //   textTheme: ButtonTextTheme.normal,
-    // ),
-    // // iconTheme: IconThemeData(color: Colors.white),
     fontFamily: 'PTSans',
   );
 }
